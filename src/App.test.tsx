1e3d30ff9fecd6cfe60bc00e8c92d3675ccf8d81
Will-Andrade <willian.santos.andra@outlook.com>
2024-02-07 18:04:51 -0400

    feat: implement the user input component and tests

---
+++ src/App.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+
+import App from "./App"
+
+it("should update the input value when a valid value is typed in", async () => {
+  const user = userEvent.setup()
+  render(<App />)
+
+  const input = await screen.findByRole("spinbutton", {
+    name: /initial investment/i,
+  })
+
+  await user.clear(input)
+  await user.type(input, "200")
+
+  expect(input.value).toBe("200")
+})
+
+it("should not update the input value when an invalid value is typed in", async () => {
+  const user = userEvent.setup()
+  render(<App />)
+
+  const input = await screen.findByRole("spinbutton", {
+    name: /expected return/i,
+  })
+
+  await user.clear(input)
+  await user.type(input, "-1")
+
+  expect(input.value).toBe("0")
+})
